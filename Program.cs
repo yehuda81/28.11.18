fea49559600c25eef36fbefadb16d1a7398b2755
yehuda81 <44754603+yehuda81@users.noreply.github.com>
2018-12-04 22:23:23 +0200

    practise1

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomGenerator = new Random();
+            Console.WriteLine("how many students in the class?");
+            int students = Convert.ToInt32(Console.ReadLine());
+            int[] cls = new int [students];
+            Double sum = 0;
+            
+            for (int i = 0; i < cls.Length; i++)
+                 {
+                    cls[i] = randomGenerator.Next(0, 100);
+                    
+                 }
+           
+
+            for (int i = 0; i < cls.Length; i++)
+            {
+                Console.WriteLine(cls[i]);
+                sum = sum + cls[i];
+            }
+            Console.WriteLine(sum);
+            Console.WriteLine(sum/cls.Length);
+
+
+
+        }
+    }
+}
